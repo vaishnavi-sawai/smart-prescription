a0212bb6ef654587b5c032d75ba0d6ea69f6878f
Gautam Verma <gautam18052001verma@gmail.com>
2023-02-17 10:37:32 +0530

    delete more files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import "./App.css"
-// import Registeration from './components/registeration/register.jsx';
-import DoctorInfo from './components/registeration/register.jsx';
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="app">
-      <div className="background" />
-      <DoctorInfo/>
-    </div>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App

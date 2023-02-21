cac3f66660d347fee9a9f6a1937946c8f9d93a61
Gautam Verma <gautam18052001verma@gmail.com>
2023-02-21 22:59:40 +0530

    more changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react';
 // import Registration from './components/Registration/Registration';
 // import Navbar  from './components/Navbar/navbar.jsx';
-import CreatePrescription from './components/CreatePrescription/CreatePrescription';
+// import CreatePrescription from './components/CreatePrescription/CreatePrescription';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
   return (
-    // <Registration/>
-    // <Navbar/>
-    <CreatePrescription/>
-  )
+  // <Registration/>
+  // <Navbar/>
+  // <CreatePrescription/>
+  <Dashboard />
+  // )
+  // const [names, setNames] = useState(['Alice', 'Bob']);
+
+  // const handleClick = () => {
+  //   const arr = ['Carl', 'Delilah'];
+
+  //   setNames(current => [...current, ...arr]);
+  // };
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={handleClick}>
+  //         Push to state array
+  //       </button>
+  //     </div>
+
+  //     {names.map((element, index) => {
+  //       return (
+  //         <div key={index}>
+  //           <h2>{element}</h2>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  );
 }
 
 export default App;
